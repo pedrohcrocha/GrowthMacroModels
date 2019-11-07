7baf75f5063def7e3483ea91df6614b806de3561
pedrohcrocha <47615056+pedrohcrocha@users.noreply.github.com>
2019-11-07 17:13:53 -0300

    Update AK_Model.R

--- AK_Model.R
+++ AK_Model.R
@@ -1,16 +1,21 @@
-# Modelo AK
+# AK model
+
+# Cobb-Douglas Hicks-Neutral Production Funcion: F(K,L) = A * K^alpha * L^(1-alpha)
 productionFunction <- function(A, K, L,alpha = 1){
   Y = A*(K^alpha)*(L^(1-alpha))
   return(Y)}
 
+# Equation of capital accumulation: dK/dt = s*F(K,L) - d*K
 capitalAcumulation <- function(s,d,A,K,L){
   Kdot = s*productionFunction(A,K,L,alpha = 1) - d*K
   return(Kdot)}
 
+# Result of the model: Income growth linearlly in a rate of g = s*A - d
 incomeGrowthRate <- function(s,A,d){
   YdotY = s*A - d
   return(YdotY)}
 
+# Data-frame with all of the results of the model
 AKmodel <- function(nrows, s, d, A, L){
   ak <- data.frame()
   for(i in 1:nrows){
@@ -24,12 +29,12 @@ AKmodel <- function(nrows, s, d, A, L){
   return(ak)
 }
 
-# Modelo 1
+# Example
 df <- AKmodel(1000, 0.4, 0.4, 10, 1)
 
-# Graficamente
+# Graphic 1
 library(ggplot2)
-  # Gráfico 1
+
 ggplot(data= df) +
   geom_line(mapping = aes(x = 1:nrow(df), y = Y), col='blue') +
   geom_line(mapping = aes(x = 1:nrow(df), y = savingsLine), col='green') +
@@ -46,7 +51,7 @@ ggplot(data= df) +
   annotate("text", x = 600, y=6200, label="Y") + 
   annotate("text", x = 600, y=2600, label="sY") + 
   annotate("text", x = 600, y=800, label="dK") + 
-  labs(title="Gráfico das equações básica do modelo AK")+
+  labs(title="Basic AK Model Equation")+
   theme(panel.background = element_blank(),
         plot.background = element_blank(),
         legend.background = element_blank(),
@@ -58,9 +63,9 @@ ggplot(data= df) +
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour = "black"),
         strip.text = element_blank(),
-        panel.spacing = unit(1, "lines")
-  )
-  # Gráfico 2
+        panel.spacing = unit(1, "lines"))
+
+# Graphic 2
 library(latex2exp)
 ggplot(data = df) +
   geom_line(mapping = aes(x = 1:nrow(df), y = ydot), col='blue') +
@@ -69,10 +74,10 @@ ggplot(data = df) +
   ylim(c(0,3.5))+
   ylab(TeX("$\\dot{Y}/Y$"))+
   xlab(TeX("$\\K$")) +
-  labs(title="Gráfico da taxa de crescimento do modelo AK")+
+  labs(title="Growth rate for AK model")+
   geom_segment(aes(x=300,xend=300, y=0.8, yend=3.2), linetype = "dashed") +
   annotate("text", x = 200, y=3.3, label="sA") + 
-  annotate("text", x = 400, y=2, label="Taxa de crescimento") + 
+  annotate("text", x = 400, y=2, label="Growth rate") + 
   annotate("text", x = 200, y=0.9, label="d") + 
   theme(panel.background = element_blank(),
         plot.background = element_blank(),
@@ -88,11 +93,10 @@ ggplot(data = df) +
         panel.spacing = unit(1, "lines")
   )
 
-# Qual é o efeito de um aumento permanente da poupança sobre a taxa de crescimento
-# e o nível de Y?
+# Effect of a permanent increase in savings
 df1 <- AKmodel(1000, 0.6, 0.8, 10, 10)
 
-# Gráfico 1
+# Graphic 1
 ggplot(data= df) +
   geom_line(mapping = aes(x = 1:nrow(df), y = Y), col='blue') +
   geom_line(mapping = aes(x = 1:nrow(df), y = savingsLine), col='green') +
@@ -116,7 +120,7 @@ ggplot(data= df) +
   labs(title="Basic AK Model equations")+
   theme_classic()
 
-# Gráfico 2
+# Graphic 2
 library(latex2exp)
 ggplot(data = df) +
   geom_line(mapping = aes(x = 1:nrow(df), y = df1$ydot), col='orange') +
@@ -126,19 +130,11 @@ ggplot(data = df) +
   ylim(c(0,6))+
   ylab(TeX("$\\dot{Y}/Y$"))+
   xlab(TeX("$\\K$")) +
-  labs(title="Gráfico da taxa de crescimento do modelo AK")+
+  labs(title="Growth rate for AK model")+
   geom_segment(aes(x=280,xend=280, y=0.8, yend=5.2), linetype = "dashed") +
   geom_segment(aes(x=300,xend=300, y=0.8, yend=3.2), linetype = "dashed") +
   annotate("text", x = 200, y=5.4, label="s'A") + 
   annotate("text", x = 200, y=3.4, label="sA") + 
-  annotate("text", x = 400, y=2, label="Taxa de crescimento") + 
+  annotate("text", x = 400, y=2, label="Growth Rate") + 
   annotate("text", x = 200, y=1, label="d") + 
   theme_classic()
-
-# Questão ANPEC
-# Considere o modelo de crescimento endógeno, com função de produção Y = AK,
-# em que Y é o produto, K é o capital e A é um índice de produtividade.
-# A taxa de poupança é de 30%. O capital deprecia à taxa de 10% ao ano. 
-# O parâmetro A é igual a 0,5 e considere n=0,02. 
-# Determine a taxa de crescimento do produto per capita.
-incomeGrowthRate(0.3, 0.5, 0.1)
